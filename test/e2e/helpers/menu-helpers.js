fce242f1d21a01e28af5e5aa7e883a9374456b51
seri0zha <sergey.ivanov@ideaplatform.ru>
2022-03-17 14:10:04 +0300

    feat(helpers): Select in menu

--- test/e2e/helpers/menu-helpers.js
+++ test/e2e/helpers/menu-helpers.js
@@ -4,38 +4,15 @@ var $h = protractor.helpers;
 var expliciteWait = $h.wait.expliciteWait;
 var angularWait = $h.wait.angularWait;
 
-exports.selectInMenu = function selectInMenu(pathArray){
-    var el = element(by.css(ROOT_SELECTOR));
-    var fullSelector = ROOT_SELECTOR;
-    function sleep(){
-        return angularWait()
+exports.selectInMenu = async (pathArray) => {
+    try {
+        for (let i = 0; i < pathArray.length; i++) {
+            if (i === 0 && pathArray.length === 1) {
+                const menuItem = await element(by.cssContainingText('.navbar-item ul.navbar-100 li a', pathArray[i]))
+                await menuItem.click();
+            }
+        }
+    } catch (e) {
+        console.error(e);
     }
-    
-    pathArray.forEach(function(currentMenuName, idx){
-        var selector = (idx > 0 ? ' ~ ul ' : '' ) + 'a[data-menu-name=\"' + currentMenuName + '\"] ';
-        fullSelector += selector;
-        $h.common.scrollToSelector(fullSelector);
-        sleep().then(() => {
-            el = element(by.css(fullSelector))
-            if(idx == pathArray.length - 1){
-                
-                el.isDisplayed().then(visibile => {
-                    if (visibile){
-                        el.click().then(sleep)
-                    }
-                    else{
-                        browser.executeScript((selector) => {
-                            $(selector).click()
-                        }, fullSelector).then(sleep)
-                    }
-                })
-            }
-            else{
-                browser.actions().mouseMove(el).perform().then(sleep)
-                
-            }
-        })
-        
-    });
-    return sleep()
 }
